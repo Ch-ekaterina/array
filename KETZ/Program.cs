3288b93418352de8ca1f392229a657dee680ed49
ekat <ekat@ekat.com>
2016-04-18 00:54:22 +0300

    суть программы в файле ТЗ.txt

---
+++ KETZ/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+
+namespace KETZ
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Random r = new Random();
+            int max = 0;
+            int[] a;
+            int[] b;
+            Console.WriteLine("Введите число:");
+
+            try
+            {
+                var n = Console.ReadLine();
+                int er = Convert.ToInt32(n);
+                a = new int[er];
+                Console.WriteLine("Массив из " + n + " элементов:");
+                int i;
+                
+                for (i = 0; i < er; i++)
+                {
+                    a[i] = r.Next(1, 10);
+                    Console.Write(a[i] + " ");
+                }
+                Console.WriteLine();
+                Console.WriteLine("Удвоенные четные элементы массива стоящие на нечетных местах:");
+                bool pr = false;
+                for (i = 0; i < er; i++)
+                {
+                    if ((i + 1)  %2 == 1)
+                    {
+                        if (a[i] % 2 == 0)
+                        {
+                            pr = true;
+                            a[i] = a[i]*2;
+                            Console.Write(a[i] + " "); 
+                        }
+                    }
+                }
+                
+                if (pr == false)
+                {
+                    Console.WriteLine("В массиве не оказалось ни одного четного элемента стоящего на нечетном месте.");
+                }
+                Console.WriteLine();
+                Console.WriteLine("Максимальное значение из нечетных элементов стоящих на четных местах:");
+
+                for (i = 0; i < er; i++)
+                {
+                    if ((i + 1)%2 == 0)
+                    {
+                        if (a[i]%2 == 1)
+                        {
+                            
+                            if (max < a[i])
+                            {
+                                max = a[i];
+                            }
+                            
+                        }
+                    }
+                }
+                if (max == 0)
+                {
+                    Console.WriteLine("В массиве не оказалось ни одного нечетного элемента стоящего на четном месте.");
+                }
+                else
+                {
+                    Console.Write(max);
+                }
+            }
+
+            catch
+            {
+                Console.WriteLine("Некорректный ввод, попробуйте воспользоваться клавишами с цифрами");
+                Program.Main(null);
+            }
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ KETZ/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Управление общими сведениями о сборке осуществляется с помощью 
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("KETZ")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("sibiryak-soft")]
+[assembly: AssemblyProduct("KETZ")]
+[assembly: AssemblyCopyright("Copyright © sibiryak-soft 2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Параметр ComVisible со значением FALSE делает типы в сборке невидимыми 
+// для COM-компонентов.  Если требуется обратиться к типу в этой сборке через 
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("8ce81b52-bcd3-4b9a-98ee-8c010ed58ded")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии 
+//   Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
